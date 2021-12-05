b492b294971de90de3ce09117b090627f92ae40c
Halder <95290415+halder1@users.noreply.github.com>
2021-12-05 11:53:28 -0500

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,10 @@
+print(5+3)
+print(12 - 4)
+print(80 /10)
+print(4 * 2)
+fav_num = 1_000
+message = fav_num
+print (message)
+import this
+#I tried inserting it into the message variable as a string, but it didn't 
+#work :C
