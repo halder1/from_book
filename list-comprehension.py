1d2af7ad8dbfcca50765714f4777f145df46069f
Halder <95290415+halder1@users.noreply.github.com>
2022-01-06 07:31:58 -0500

    Add files via upload
    Files from January 6 2022

---
+++ list-comprehension.py
@@ -0,0 +1,19 @@
+cubes = [cubed**3 for cubed in range(1, 100)]
+print(cubes)
+
+#So as far as I can tell, this creates a list "cubes", then defines it by 
+#assigning a variable "cubed" to every number from 1 to 100. The for loop here
+#then somehow knows how to use the cubed**3 (such to say, that, it will cube
+#every number from 1-100). This is as opposed to writing the following code,
+#which will take up more lines
+
+	#cubes = []
+	#for cubed in range(1,100):
+		#cubed = cubed**3
+		#cubes.append(cubed)
+	#print(cubes)
+
+#This is five lines of code as opposed to two, which isn't favorable. Although 
+#they produce the same result, the first option is smaller and therefore more
+#efficient. The top is known as a list comprehension.
+
